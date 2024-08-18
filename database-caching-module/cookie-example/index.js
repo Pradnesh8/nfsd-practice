10622140aa22db56865df2f0ba06a050ae6fcaf9
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2024-08-19 00:47:56 +0530

    cookie example

---
+++ database-caching-module/cookie-example/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}\\client\\index.html`)
+})
+
+app.get("/logout", (req, res) => {
+    res.setHeader('Clear-Site-Data', '"cookie","cache","storage"')
+    res.redirect("/");
+})
+
+app.post("/set-preferences", (req, res) => {
+    const data = req.body.preferences
+    res.cookie('userPreferences', data, { maxAge: 3600000 })
+    res.redirect("/");
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log("Server is running at port " + PORT);
+})
